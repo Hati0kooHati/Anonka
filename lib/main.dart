65bc35f1d6648e71319ab7718631ae797587aa56
Hati0kooHati <dserion.prn@gmail.com>
2025-11-26 21:49:58 +0600

    relocate firebase_options into lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:anonka/src/core/app/app_screen.dart';
-import 'package:anonka/src/firebase_options.dart';
+import 'package:anonka/firebase_options.dart';
 import 'package:anonka/src/core/injection/inject.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
